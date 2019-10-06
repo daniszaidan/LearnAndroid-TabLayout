d9f5bec102925174d03a0653c2af1bf2c2691cdf
Danis Zaidan <daniszaidan@gmail.com>
2019-10-06 15:57:28 +0700

    Scroll Tab Fix Uhuy

---
+++ app/src/main/java/org/d3ifcool/tablayout/ConstraintTabActivity.java
@@ -0,0 +1,74 @@
+package org.d3ifcool.tablayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConstraintTabActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_tab);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager2);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout2);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new OneFragment(), "Senin");
+        adapter.addFragment(new TwoFragment(), "Selasa");
+        adapter.addFragment(new ThreeFragment(), "Rabu");
+        adapter.addFragment(new FourFragment(), "Kamis");
+        adapter.addFragment(new FiveFragment(), "Jum'at");
+        adapter.addFragment(new SixFragment(), "Sabtu");
+        adapter.addFragment(new SevenFragment(), "Minggu");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+}
