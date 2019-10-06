6860d33bd82a30215735743c2afa2c0271eb4732
Danis Zaidan <daniszaidan@gmail.com>
2019-10-06 13:22:37 +0700

    Initial commit

---
+++ app/src/main/java/org/d3ifcool/tablayout/MainActivity.java
@@ -0,0 +1,13 @@
+package org.d3ifcool.tablayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
